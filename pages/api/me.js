4f0b463e0f41d39b94c21640181a48c2f8d93a18
nunu111 <nunu111za@hotmail.com>
2022-09-22 15:14:51 +0700

    lab10 t2

--- pages/api/me.js
+++ pages/api/me.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  return res.status(404).json({ "Error Message": "Request not found" });
+  return res.json({ name: "Phanuwat Ngoenthok", studentId: "640610659" });
 }
